9241c03c819cfe9bcda959bf7fa4487128a41a38
Carlos <carlosrb206@gmail.com>
2021-09-17 13:23:38 -0400

    submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 
-var createTashHandler = function() {
+var createTaskhHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = 'This is a new task.';
     tasksToDoEl.appendChild(listItemEl);
+    
+    console.log(event);
 }
 
-buttonEl.addEventListener('click', createTashHandler);
+formEl.addEventListener('submit', createTaskhHandler);
