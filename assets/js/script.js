8bfcfe71d2c8c9f1cb4e50d0e0251ab372d59905
Carlos <carlosrb206@gmail.com>
2021-09-21 22:33:30 -0400

    add array of objects for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 
+var tasks = [];
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -28,7 +29,8 @@ var taskFormHandler = function(event) {
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
-        type: taskTypeInput
+        type: taskTypeInput,
+        
     };
 
     var isEdit = formEl.hasAttribute("data-task-id");
@@ -42,7 +44,8 @@ var taskFormHandler = function(event) {
     else {
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
     };
   
     createTaskEl(taskDataObj);
@@ -56,6 +59,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
     // set new values
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    // loop through tasks array and task object with new content
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+        tasks[i].name = taskName;
+        tasks[i].type = taskType;
+        }
+    };
 
     alert("Task Updated!")
 
@@ -79,12 +90,19 @@ var createTaskEl = function(taskDataObj) {
     
     listItemEl.appendChild(taskInfoEl);
 
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
+
     var taskActionEl = createTaskAction(taskIdCounter);
     listItemEl.appendChild(taskActionEl);
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
     //increase task counter for next unique id
     taskIdCounter++;
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
 }
 
 var createTaskAction = function(taskId) {
@@ -196,6 +214,14 @@ var taskStatusChangeHandler = function(event) {
       else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
       }
+
+    // update task's in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+        tasks[i].status = statusValue;
+        }
+    }
+    console.log(tasks);
 
 };
 
